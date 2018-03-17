c3e40c012069ad9b8a140e156f59c7b1b018096d
blue enigma <bluenigma@protonmail.com>
2018-03-17 23:48:23 +0100

    some changes

--- autom.py
+++ autom.py
@@ -4,20 +4,15 @@ import re
 import getpass
 from clm import rd
 
-target_server = 'imap.gmail.com'
-target_email = "jdfetpy@gmail.com"
-target_pwd = getpass.getpass(f'Password for {target_email}: ')
 
 
 a = []
 
-
-
-
 def fetch_email():
+    target_server = 'imap.gmail.com'
+    target_email = "jdfetpy@gmail.com"
+    target_pwd = getpass.getpass(f'Password for {target_email}: ')
     raw_messages_list = []
-    
-    
     try:
         print(f"Logging in to {target_server}")
         mail = imaplib.IMAP4_SSL(target_server)
@@ -26,30 +21,27 @@ def fetch_email():
         mail.login(target_email,target_pwd)
         print('OK')
         mail.select()
-        
-        
-        
         typ, data = mail.search(None, 'ALL')
-        
-        
-        
         for num in data[0].split():
             typ, content = mail.fetch(num, '(RFC822)')
             content_string = str(content[0][1])
             raw_messages_list.append(content_string)
-
-        
         mail.close()
         mail.logout()
-        
         return raw_messages_list
-    
-    
-
     except Exception as e:
         print('Something went wrong. ')
         print(f'Error: {e}')
-        
+
+def translate_date(sdate):
+    bls1 = sdate.split('/')
+    bls1.reverse()
+    step = '20' + bls1.pop(0)
+    bls1.insert(0,step)
+    final = str(bls1[0] + '-' + bls1[1] + '-' + bls1[2])
+    return final
+    
+    
 
 def translate_mails(indata):
     b = {}
@@ -61,12 +53,8 @@ def translate_mails(indata):
     send_date = None
     color_total = None
     black_total = None
-    
-    
     for message in indata:
          listed_data.append(message.split('\\r\\n'))
-         
-    
     for i in range(0,len(listed_data)):    
         for record in listed_data[i]:
             if re.search(r'Model Name',record):
@@ -79,8 +67,9 @@ def translate_mails(indata):
                 b['sn'] = sn
             elif re.search(r'Send Date',record):
                 send_date = record.strip("[Send Date],")
-                print(send_date)
-                b['send date'] = send_date
+                date_t = translate_date(send_date)
+                print(date_t)
+                b['send date'] = date_t
             elif re.search(r'Total Color Counter',record):
                 color_total = int(record.strip("[Total Color Counter],"))
                 print(color_total)
@@ -89,7 +78,7 @@ def translate_mails(indata):
                 black_total = int(record.strip("[Total Black Counter],"))
                 print(black_total)
                 b['black total'] = black_total
-                a.append(rd(sn, name, send_date, color_total, black_total))
+                a.append(rd(sn, name, date_t, color_total, black_total))
             else:
                 pass
         
