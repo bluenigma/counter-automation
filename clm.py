f1ecaa2e7b83bffdd1bf2e8d8670f7b1717afb1a
blue enigma <bluenigma@protonmail.com>
2018-03-22 01:07:03 +0100

    removed unnecessary clm.py

--- clm.py
+++
@@ -1,9 +0,0 @@
-class rd():
-    
-    def __init__(self, sn, name, date, color_total, black_total):
-        self.name = name
-        self.sn = sn
-        self.date = date
-        self.color_total = color_total
-        self.black_total = black_total
-        print(f'{self.name} instance created')
