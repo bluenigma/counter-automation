518f3c6e97f955f6855e0d833ee39708d55d9e5d
blue enigma <bluenigma@protonmail.com>
2018-06-26 01:13:42 +0200

    modify units

--- copier_manage/copins.py
+++ copier_manage/copins.py
@@ -21,12 +21,6 @@ def add_unit():
     'client_id':None,
     }
 
-    # new_unit = Unit(
-    # sn = input_data['sn'],
-    # model = input_data['model'],
-    # black_count = input_data['black_count'],
-    # color_count = input_data['color_count'],
-    # )
     while True:
         cnfrm = input('Confirm new unit? (y/n)\n')
         if cnfrm == 'y':
@@ -60,7 +54,9 @@ def select_units():
         print("No unit matching specified criteria.")
         select_units()
     elif len(qry) == 1:
+        print("-"*10)
         print(f"Selected {qry[0].sn} | {qry[0].model}")
+        print("-"*10)
         selection_unit.append(qry[0])
         select_units()
     elif len(qry) > 1:
@@ -72,18 +68,71 @@ def select_units():
         raise Exception("Error in unit selection, starting over.")
         select_units()
 
-def modify_unit():
-    pass
-
 
 def clear_unit_selection():
     print("Clearing all units from selection.")
     selection_unit.clear()
 
-def select_client():
-    criteria = input("YO!Search client or hit RETURN:\n> ")
-    if len(criteria) == 0:
+# --------------------------Modify units---------------------------
+
+def unit_modify_sn(target):
+    # Can't be implemented until database schema is changed. sn is the primary
+    # key for the table.
+    pass
+
+def unit_modify_model(target):
+    print("This function let's you modify copier's model.\n")
+    val = input("Enter new model:\n> ")
+    for unit in target:
+        unit.model = val
+        session.commit()
+
+def unit_modify_bk(target):
+    print("This function let's you modify copier's black count.\n")
+
+    try:
+        val = int(input("Enter new black counter:\n> "))
+    except ValueError:
+        print("Enter a valid number. Starting over.")
+    for unit in target:
+        unit.black_count = val
+        session.commit()
+
+
+def unit_modify_col(target):
+    print("This function let's you modify copier's color count.\n")
+    try:
+        val = int(input("Enter new color counter:\n> "))
+    except ValueError:
+        print("Enter a valid number. Starting over.")
+        unit_modify_col()
+
+    for unit in target:
+        unit.color_count = val
+        session.commit()
+
+
+def unit_modify_client_id(target):
+    print("This function let's you assign copier or copiers to a client.\n")
+    cli = select_client()
+    if cli == "Cancelled.":
         return
+    else:
+        pass
+
+    for unit in target:
+        unit.client_id = int(cli.ID)
+        session.commit()
+
+
+
+
+
+# --------------------------------------------------------------
+def select_client():
+    criteria = input("Select a client or hit RETURN:\n> ")
+    if len(criteria) == 0:
+        return("Cancelled.")
     else:
         pass
     qry = (session.query(Client).filter(Client.name.ilike
@@ -92,10 +141,11 @@ def select_client():
         print("No client matching specified criteria.")
         select_client()
     elif len(qry) == 1:
-        selection_client.append(qry[0])
+        # selection_client.append(qry[0])
         print(f'Selected {qry[0].name}. Client ID: {qry[0].ID}')
+        return(qry[0])
     elif len(qry) > 1:
-        print("Multiple units matching:")
+        print("Multiple clients matching:")
         for result in qry:
             print(f'ID: {result.ID} | {result.name}')
         print('Narrow selection to one client.')
@@ -105,5 +155,10 @@ def clear_client_selection():
     print("Clearing client selection.")
     selection_client.clear()
 
+# ---------------------------------------------------------------
+select_units()
+unit_modify_client_id(selection_unit)
+
+# ---------------------------------------------------------------
 if __name__ == '__main__':
     pass
