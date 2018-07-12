4f4ef6bab62f47616de31936ae39168a08d8c60c
blue enigma <bluenigma@protonmail.com>
2018-07-12 20:43:26 +0200

    fix custins id line

--- counter_automation/custins.py
+++ counter_automation/custins.py
@@ -4,6 +4,9 @@ from db_init import Client, Base
 from sys import argv
 import csv
 
+# Script that populates clients table in database
+# uses a csv file as an argument
+
 script, csvfile = argv
 
 engine = create_engine('sqlite:///counter_autom.db')
@@ -14,14 +17,18 @@ session = DBSession()
 def populate_clients():
     with open(csvfile, 'r') as infile:
         reader = csv.reader(infile)
-
         next(reader)
         for row in reader:
-            new_client = Client(ID=row[0],name=row[1],black_rate=row[2],color_rate=row[3],min_monthly_pay=row[4])
+            new_client = Client(
+                                id=row[0],
+                                name=row[1],
+                                black_rate=row[2],
+                                color_rate=row[3],
+                                min_monthly_pay=row[4]
+                                )
             session.add(new_client)
             session.commit()
 
 
 if __name__ == '__main__':
     populate_clients()
-    
